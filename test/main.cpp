56643dd76b110d87d4f996e5991427317ee5bbf1
aeoleader <aeoleader@gmail.com>
2015-09-17 21:49:08 -0400

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Xiangyu on 9/17/15.
+//  Copyright (c) 2015 Xiangyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
